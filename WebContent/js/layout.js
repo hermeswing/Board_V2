080eacc70d0cf75b08c7f615b24d47467304333f
Administrator <Administrator@hermeswing-PC>
2014-04-01 20:04:32 +0900

    게시판 버젼 2 수정 - 박종영

---
+++ WebContent/js/layout.js
@@ -0,0 +1,52 @@
+// JavaScript Document
+function fixed(){
+	document.getElementById('xe').className='fixed';
+}
+function liquid(){
+	document.getElementById('xe').className='liquid';
+}
+function hybrid(){
+	document.getElementById('xe').className='hybrid';
+}
+function c(){
+	document.getElementById('container').className='c';
+	document.getElementById('preview').src='img/c.gif';
+}
+function ce(){
+	document.getElementById('container').className='ce';
+	document.getElementById('preview').src='img/ce.gif';
+}
+function ec(){
+	document.getElementById('container').className='ec';
+	document.getElementById('preview').src='img/ec.gif';
+}
+function cee(){
+	document.getElementById('container').className='cee';
+	document.getElementById('preview').src='img/cee.gif';
+}
+function ece(){
+	document.getElementById('container').className='ece';
+	document.getElementById('preview').src='img/ece.gif';
+}
+function eec(){
+	document.getElementById('container').className='eec';
+	document.getElementById('preview').src='img/eec.gif';
+}
+function img_c(){
+	document.getElementById('preview').src='img/c.gif';
+}
+function img_ce(){
+	document.getElementById('preview').src='img/ce.gif';
+}
+function img_ec(){
+	document.getElementById('preview').src='img/ec.gif';
+}
+function img_cee(){
+	document.getElementById('preview').src='img/cee.gif';
+}
+function img_ece(){
+	document.getElementById('preview').src='img/ece.gif';
+}
+function img_eec(){
+	document.getElementById('preview').src='img/eec.gif';
+}
